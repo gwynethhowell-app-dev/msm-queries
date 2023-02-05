5a939a8f472130b4ef91d94f273c934212041802
gwynethhowell <ghowell@uchicago.edu>
2023-02-05 22:54:37 +0000

    director almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index"})
+  get("/directors/eldest", { :controller => "directors", :action => "old"})
+  get("/directors/youngest", { :controller => "directors", :action => "young"})
+
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details"})
+
 end
